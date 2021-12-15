f2fc70ef972cc9e205492419815a113c5d3a9244
yamamurodaisuke <daisuke0308@icloud.com>
2021-12-15 16:08:51 +0900

    グリーンフェーズ

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+class VacancyLevel
+{
+    private $remainingCount;
+
+    public function __construct(int $remainingCount)
+    {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function mark(): string
+    {
+        if ($this->remainingCount === 0) {
+            return '×';
+        }
+        if ($this->remainingCount === 4) {
+            return '△';
+        }
+        if ($this->remainingCount === 5) {
+            return '◎';
+        }
+    }
+}
