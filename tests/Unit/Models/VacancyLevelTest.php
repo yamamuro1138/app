7cfc230e080102fd06a6cfe8f931db04546ac6cd
yamamurodaisuke <daisuke0308@icloud.com>
2021-12-15 16:39:01 +0900

    テスト

--- tests/Unit/Models/VacancyLevelTest.php
+++ tests/Unit/Models/VacancyLevelTest.php
@@ -7,15 +7,32 @@ use App\Models\VacancyLevel;
 
 class VacancyLevelTest extends TestCase
 {
-    public function testMark()
+ /**
+     * @param int $remainingCount
+     * @param string $expectedMark
+     * @dataProvider dataMark
+     */
+    public function testMark(int $remainingCount, string $expectedMark)
     {
-        $level = new VacancyLevel(0);
-        $this->assertSame('×', $level->mark());
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedMark, $level->mark());
+    }
 
-        $level = new VacancyLevel(4);
-        $this->assertSame('△', $level->mark());
-
-        $level = new VacancyLevel(5);
-        $this->assertSame('◎', $level->mark());
+    public function dataMark()
+    {
+        return [
+            '空きなし' => [
+                'remainingCount' => 0,
+                'expectedMark' => '×',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => '△',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => '◎',
+            ],
+        ];
     }
 }
