b27912a95c1581c256c553c33b5e25006c92dc79
yamamurodaisuke <daisuke0308@icloud.com>
2021-12-27 16:50:41 +0900

    テストコードをデータベースを使用するように変更

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Lesson::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'coach_name' => $faker->name,
+        'capacity' => $faker->randomNumber(2),
+        'start_at' => $faker->dateTime,
+        'end_at' => $faker->dateTime,
     ];
 });
